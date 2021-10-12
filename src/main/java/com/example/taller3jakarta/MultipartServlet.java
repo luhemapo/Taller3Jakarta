052638080340246be8d778cb494014e9d4f0cb58
pipe1990 <82337240+pipe1990@users.noreply.github.com>
2021-10-12 15:59:55 -0500

    Add files via upload
    multipart servlet

---
+++ src/main/java/com/example/taller3jakarta/MultipartServlet.java
@@ -0,0 +1,51 @@
+package co.edo.unbosque.jakartaeenew.jakartaeenew;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "multiPartServlet", value = "/multiPartServlet")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+    maxFileSize = 1024 * 1024 * 5,
+    maxRequestSize = 1024 * 1024 * 5 * 5)
+
+public class MultipartServlet extends HttpServlet {
+    private String message;
+    private String UPLOAD_DIRECTORY = "uploads";
+
+    public void init() {
+    message = "hola";
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+        response.setContentType("text/html");
+
+       String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) uploadDir.mkdir();
+
+        String fileName = "";
+
+        try {
+            for (Part part : request.getParts()) {
+                fileName = part.getSubmittedFileName();
+                part.write(uploadPath + File.separator + fileName);
+            }
+            request.setAttribute("message", "El archivo se ha subido correctamente!" + fileName);
+        } catch (FileNotFoundException  e) {
+            e.printStackTrace();
+        }
+        getServletContext().getRequestDispatcher("/result.jsp").forward(request, response);
+
+    }
+
+    public void destroy() {
+    }
+}
