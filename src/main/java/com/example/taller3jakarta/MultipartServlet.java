3c3d6e1c6f6b673d5235c35cfa635c0fbb099c6f
luis <lmartinezpov@unbosque.edu.co>
2021-10-12 16:51:43 -0500

    project

--- src/main/java/com/example/taller3jakarta/MultipartServlet.java
+++ src/main/java/com/example/taller3jakarta/MultipartServlet.java
@@ -14,8 +14,8 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "multiPartServlet", value = "/multiPartServlet")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024,
-    maxFileSize = 1024 * 1024 * 5,
-    maxRequestSize = 1024 * 1024 * 5 * 5)
+    maxFileSize = 1024 * 1024 * 10,
+    maxRequestSize = 1024 * 1024 * 5 * 10)
 
 public class MultipartServlet extends HttpServlet {
     private String message;
